1848e788db7eeba61a621bfbf1616081484213d7
melprog <cantoumelanie@gmail.com>
2024-09-12 13:18:43 -0300

    Primer commit

--- js/categories.js
+++ js/categories.js
@@ -142,13 +142,3 @@ document.addEventListener("DOMContentLoaded", function(e){
     });
 });
 
-window.onload = function() {
-    if (localStorage.getItem("autenticado") === "true") {
-        let userName = localStorage.getItem('username');
-        if (userName) {
-            document.getElementById("user-email").textContent = userName;
-        } 
-        } else {
-            window.location.href = 'login.html';
-    }
-}
