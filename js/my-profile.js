30597eea9c9374279274ab47f16e802c8710b6b0
florenjohanson <flore.johanson@gmail.com>
2024-10-17 16:42:12 -0300

    Flor-errores 17

--- js/my-profile.js
+++ js/my-profile.js
@@ -4,6 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const navProfileImage = document.getElementById("navProfileImage"); // Imagen de perfil en la barra de navegación
   const saveProfileButton = document.getElementById("saveProfile");
   const alertContainer = document.getElementById("alertContainer"); // Referencia al contenedor de alerta
+  
+  function loadProfileData(){
+let email=localStorage.getItem("username");
+let userInfo=localStorage.getItem("datosFormulario");
+if (userInfo){
+  let userInfoUserObject=json.parse(userInfo);
+ document.getElementById("nombre").value = userInfoUserObject.nombre;
+  document.getElementById("segundo_nombre").value= userInfoUserObject.segundo_nombre;
+  // document.getElementById("apellido").value;
+  // document.getElementById("segundo_apellido").value;
+  // document.getElementById("email").value;
+  // document.getElementById("telefono").value;
+
+}
+  }
+
+  
 
   // Tamaño máximo permitido para la imagen (en bytes), por ejemplo, 2 MB
   const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2 MB en bytes
@@ -62,12 +79,13 @@ document.addEventListener("DOMContentLoaded", () => {
       const imageData = profileImage.src;
       localStorage.setItem("profileImage", imageData);
     }
-
+    guardarDatos();
     showSuccessAlert();
   });
 
   // Cargar la imagen guardada cuando se abra la página
   loadProfileImage();
+ loadProfileData();
 });
 
 
@@ -99,4 +117,34 @@ inputEmail.addEventListener('blur', () => {
         alert('Por favor, ingresa una dirección de correo electrónico válida.');
     }
 });
+
+function guardarDatos() {
+  // Obtener los valores de los campos (suponiendo que tienes campos con ID "nombre", "email" y "mensaje")
+  const nombre = document.getElementById("nombre").value;
+  const segundo_nombre = document.getElementById("segundo_nombre").value;
+  const apellido = document.getElementById("apellido").value;
+  const segundo_apellido = document.getElementById("segundo_apellido").value;
+  const email = document.getElementById("email").value;
+  const telefono = document.getElementById("telefono").value;
+
+
+
+  // Crear un objeto con los datos
+  const datosFormulario = {
+    nombre: nombre,
+    segundo_nombre: segundo_nombre,
+    apellido: apellido,
+    segundo_apellido: segundo_apellido,
+    email: email,
+    telefono: telefono,
+    
+
+    
+  };
+
+
+  
+  // Convertir el objeto a JSON y almacenarlo en Local Storage
+  localStorage.setItem("datosFormulario", JSON.stringify(datosFormulario));
+}
 
